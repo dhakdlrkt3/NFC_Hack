e74fa3afb4037b14095e6b78bb509965b529137b
Yoo <dhakdlrkt3@naver.com>
2018-11-16 23:17:19 +0900

    NFC read성공

--- app/src/main/java/com/example/HackerTon/NFC_Hack/Idinfo.java
+++ app/src/main/java/com/example/HackerTon/NFC_Hack/Idinfo.java
@@ -14,7 +14,6 @@ import android.nfc.tech.Ndef;
 import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -24,9 +23,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import static android.content.Intent.getIntent;
 
 
 public class Idinfo extends AppCompatActivity {
@@ -35,11 +38,14 @@ public class Idinfo extends AppCompatActivity {
     private boolean mResumed = false;
     private boolean mWriteMode = false;
     NfcAdapter mNfcAdapter;
+    NfcAdapter nfcAdapter;
     EditText mNote, mNote2,mNote3,mNote4,mNote5;
-
+    PendingIntent pendingIntent;
     PendingIntent mNfcPendingIntent;
     IntentFilter[] mWriteTagFilters;
     IntentFilter[] mNdefExchangeFilters;
+    TextView tvNFCContent;
+    IntentFilter writeTagFilters[];
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -81,8 +87,67 @@ public class Idinfo extends AppCompatActivity {
         // Intent filters for writing to a tag
         IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
         mWriteTagFilters = new IntentFilter[] { tagDetected };
+
+
+
+        //여기부터 시작
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        /*if (nfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+        } 예외처리*/
+
+        readFromIntent(getIntent());
+
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter tagfDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        tagfDetected.addCategory(Intent.CATEGORY_DEFAULT);
+        writeTagFilters = new IntentFilter[] { tagfDetected };
     }
 
+
+
+    /******************************************************************************
+     **********************************Read From NFC Tag***************************
+     ******************************************************************************/
+    private void readFromIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs = null;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+            buildTagViews(msgs);
+        }
+    }
+    private void buildTagViews(NdefMessage[] msgs) {
+        if (msgs == null || msgs.length == 0) return;
+
+        String text = "";
+//        String tagId = new String(msgs[0].getRecords()[0].getType());
+        byte[] payload = msgs[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16"; // Get the Text Encoding
+        int languageCodeLength = payload[0] & 0063; // Get the Language Code, e.g. "en"
+        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+
+        try {
+            // Get the Text
+            text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        } catch (UnsupportedEncodingException e) {
+            Log.e("UnsupportedEncoding", e.toString());
+        }
+
+        tvNFCContent.setText("NFC Content: " + text);
+    }
+
+    //여기까지 붙임
     @Override
     protected void onResume() {
         super.onResume();
